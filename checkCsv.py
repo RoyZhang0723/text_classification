f4e17544a91f01fc8c6f78b518d133289a333e32
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-11-07 14:51:22 +0800

    合并两个csv文件

--- checkCsv.py
+++ checkCsv.py
@@ -2,11 +2,17 @@ import csv
 with open(r'savedrecs.csv', 'r', newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     headers = next(csv_reader)  # 获取第一行，可能是头
-    for i in headers:
-        print(i)
-    print(headers)
-
-
+    with open(r'savedrecsA.csv', 'w', newline='') as file:
+        fileWriter = csv.writer(file, delimiter=',')
+        for row in csv_reader:
+            fileWriter.writerow(row)
+#把第二个csv文件续写进来，合并
+with open(r'savedrecs 2.csv', 'r', newline='') as csvfile2:
+    csv_reader2 = csv.reader(csvfile2, delimiter=',')
+    with open(r'savedrecsA.csv', 'a', newline='') as file:
+        fileWriter = csv.writer(file, delimiter=',')
+        for row in csv_reader2:
+            fileWriter.writerow(row)
 
     # 每一行索引用
     # for row in csv_reader:
