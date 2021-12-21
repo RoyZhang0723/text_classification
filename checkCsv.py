e8ebc10d59230da6c223eb7b33954ddb0dae3eb3
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-12-21 16:17:42 +0800

    stakeholders相关数据(合并超出1000的数据)

--- checkCsv.py
+++ checkCsv.py
@@ -1,15 +1,15 @@
 import csv
-with open(r'savedrecs.csv', 'r', newline='') as csvfile:
+with open(r'NLP_data/Contractor(1).csv', 'r', newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     headers = next(csv_reader)  # 获取第一行，可能是头
-    with open(r'savedrecsA.csv', 'w', newline='') as file:
+    with open(r'NLP_data/Contractor.csv', 'w', newline='') as file:
         fileWriter = csv.writer(file, delimiter=',')
         for row in csv_reader:
             fileWriter.writerow(row)
 #把第二个csv文件续写进来，合并
-with open(r'savedrecs 2.csv', 'r', newline='') as csvfile2:
+with open(r'NLP_data/Contractor(2).csv', 'r', newline='') as csvfile2:
     csv_reader2 = csv.reader(csvfile2, delimiter=',')
-    with open(r'savedrecsA.csv', 'a', newline='') as file:
+    with open(r'NLP_data/Contractor.csv', 'a', newline='') as file:
         fileWriter = csv.writer(file, delimiter=',')
         for row in csv_reader2:
             fileWriter.writerow(row)
