4f7f4e428f0a2ae0b9e157c2ddce53c3eddb7047
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-12-22 19:18:59 +0800

    stakeholders相关数据(修改一处数据为完整记录)，改为在dataframe处合并

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ from transformers import BertTokenizer, BertModel, BertForMaskedLM
 import csv
 import pandas as pd
 import os
+
+# refine data
 dfs = []
 files = os.walk(r"NLP_data")
 for path, dir_list, file_list in files:
     for file_name in file_list:
         print(os.path.join(path, file_name))
         dfs.append(pd.read_csv(os.path.join(path, file_name), delimiter="\t"))
-contractorAB1 = dfs[0]['AB']
-mediaAB = dfs[1]['AB']
-greenGroupAB = dfs[2]['AB']
-governmentAB = dfs[3]['AB']
-subContractorAB = dfs[4]['AB']
-politicalPartyAb = dfs[5]['AB']
-NGOAB = dfs[6]['AB']
-workerAB = dfs[7]['AB']
-consultantAB = dfs[8]['AB']
-surveyorAB = dfs[9]['AB']
-environmentalGroupAB = dfs[10]['AB']
-communityAB = dfs[11]['AB']
-residentAB = dfs[12]['AB']
-designerAB = dfs[13]['AB']
-contractorAB2 = dfs[14]['AB']
+contractorAB1 = dfs[0]['ID']
+mediaAB = dfs[1]['ID']
+greenGroupAB = dfs[2]['ID']
+governmentAB = dfs[3]['ID']
+subContractorAB = dfs[4]['ID']
+politicalPartyAb = dfs[5]['ID']
+NGOAB = dfs[6]['ID']
+workerAB = dfs[7]['ID']
+consultantAB = dfs[8]['ID']
+surveyorAB = dfs[9]['ID']
+environmentalGroupAB = dfs[10]['ID']
+communityAB = dfs[11]['ID']
+residentAB = dfs[12]['ID']
+designerAB = dfs[13]['ID']
+contractorAB2 = dfs[14]['ID']
 contractorAB = pd.concat([contractorAB1, contractorAB2], axis=0)
-print(contractorAB)
 
-# csvFile = 'NLP_data/Community.csv'
-# df = pd.read_csv(csvFile, delimiter="\t")
-# print(df['AB'])
+
+
+
+
